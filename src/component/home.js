2a9d8633a4c5cf9cc8e9488a549d234d220dcb97
Mohit Satija <70685094+EH-GD-MOHIT21@users.noreply.github.com>
2021-08-24 12:48:43 +0530

    Update home.js

--- src/component/home.js
+++ src/component/home.js
@@ -7,7 +7,7 @@ import ScriptTag from 'react-script-tag';
 export default function Home() {
     return (
         <div className="homecom" id="homecom">
-            <ScriptTag type="text/javascript" src="http://127.0.0.1:8000/static/particlejs/typedm.js"/>
+            <ScriptTag type="text/javascript" src="https://mohitsatija.herokuapp.com/static/particlejs/typedm.js"/>
             <PersistentDrawerLeft hover="Home"/>
             <MyCard/>
         </div>
